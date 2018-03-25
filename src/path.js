22c8eacaef4cc9e25743e87c8231be4912da0230
Graceqi <1792197177@qq.com>
2018-03-25 10:34:34 +0800

    把所有接口地址都拿到path.js中

---
+++ src/path.js
@@ -0,0 +1,16 @@
+//登录login页面
+var login_path='http://localhost:5050/api/account/login';
+//var login_path='http://localhost:5050/gm/account/login';
+
+//注册新用户createnewuser页面
+var register_path='http://localhost:5050/api/account/register';
+//var register_path='http://localhost:5050/gm/account/register';
+//var register_path='http://hr.yuanw.wang:5000';
+
+//企业信息管理enterpriseinformationmanagement页面
+var companymanage_path='http://localhost:5050/api/info/record';
+//var companymanage_path='http://localhost:5050/gm/account/companyinfo';
+
+//enterprisedataselect页面和enterprisedataupload页面
+var companyupload_path='http://localhost:5050/gm/account/enterprisedataupload';
+
