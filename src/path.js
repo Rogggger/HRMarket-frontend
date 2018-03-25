4502faca511b47c7f60cb4f63aff11f75c6bc71e
Graceqi <1792197177@qq.com>
2018-03-25 21:54:28 +0800

    不可修改

--- src/path.js
+++ src/path.js
@@ -1,2 +1,3 @@
-const base_path='https://hr.yuanw.wang/api/';
+//const base_path='https://hr.yuanw.wang/api/';
 //const base_path='http://localhost:5050/gm/';
+const base_path='http://localhost:5050/api/';
