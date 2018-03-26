c7e32ce159bc9a33a49823816240f558510cf193
Graceqi <1792197177@qq.com>
2018-03-26 10:25:19 +0800

    修改网址

--- src/path.js
+++ src/path.js
@@ -1,3 +1,3 @@
-//const base_path='https://hr.yuanw.wang/api/';
-const base_path='http://localhost:5050/gm/';
+const base_path='https://hr.yuanw.wang/api/';
+//const base_path='http://localhost:5050/gm/';
 //const base_path='http://localhost:5050/api/';
